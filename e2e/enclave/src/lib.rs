943a0a2ccae0020af525b57fd7e6fe8ef671e5de
Jun Kimura <jun.kimura@datachain.jp>
2024-06-29 16:48:43 +0900

    remove dependencies
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- e2e/enclave/src/lib.rs
+++ e2e/enclave/src/lib.rs
@@ -2,8 +2,6 @@
 extern crate alloc;
 
 use enclave_runtime::{setup_runtime, Environment, MapLightClientRegistry};
-// NOTE: You must use `preset::mainnet` instead of `preset::minimal` in testnets(goerli,sepolia,holesky) or mainnet.
-use ethereum_elc::ibc::consensus::preset::minimal::PRESET;
 
 setup_runtime!({
     simple_logger::SimpleLogger::new()
@@ -15,8 +13,6 @@ setup_runtime!({
 
 fn build_lc_registry() -> MapLightClientRegistry {
     let mut registry = MapLightClientRegistry::new();
-    tendermint_lc::register_implementations(&mut registry);
-    ethereum_elc::register_deneb_implementations::<{ PRESET.SYNC_COMMITTEE_SIZE }>(&mut registry);
     besu_qbft_elc::register_implementations(&mut registry);
     registry
 }
