0edb5e5f3a666ecca43c68748e5f6f8bcbb4593d
Jun Kimura <jun.kimura@datachain.jp>
2024-06-29 13:09:00 +0900

    fix e2e script
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- e2e/scripts/run_e2e_test.sh
+++ e2e/scripts/run_e2e_test.sh
@@ -10,8 +10,6 @@ export RLY_BIN
 CERTS_DIR=./certs
 
 ./scripts/init_lcp.sh
-source ./chain0.env.sh && ./relayer/scripts/gen-config.sh ibc0
-source ./chain1.env.sh && ./relayer/scripts/gen-config.sh ibc1
 
 if [ "$SGX_MODE" = "SW" ]; then
     export LCP_RA_ROOT_CERT_HEX=$(cat ${CERTS_DIR}/root.crt | xxd -p -c 1000000)
@@ -20,6 +18,10 @@ fi
 make network
 sleep 3
 make deploy-contracts
+
+./scripts/extract-abi.sh
+source ./chain0.env.sh && ./relayer/scripts/gen-config.sh ibc0
+source ./chain1.env.sh && ./relayer/scripts/gen-config.sh ibc1
 
 ${LCP_BIN} --log_level=info service start --enclave=${ENCLAVE_PATH} --address=127.0.0.1:50051 --threads=2 &
 LCP_PID=$!
